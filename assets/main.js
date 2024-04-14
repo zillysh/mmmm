6b9c10f432ad559cf0ed44367d8c67e45fe29284
Zil Shah <shahz134@newschool.edu>
2024-04-13 22:54:41 -0400

    Working Filters

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,7 @@
 
 
+let items = [];
+
 // Number of Results
   const updateResultsText = (count) => {
   const resultsText = document.getElementById('results-text');
@@ -28,16 +30,33 @@ const renderSingleItem = (item) => {
   updateResultsText(data.length);
 };
 
+let searchFilter = {
+  mealType: '',
+  cuisineType: '',
+  cookTime: '',
+  ingredients: ''
+}
 
 // Function to render items based on meal type
-  const renderMealsByType = (data, mealType) => {
-  const filteredMeals = data.filter(item => item.mealType.includes(mealType));
+  const renderMeals = (data, filter) => {
+  let filteredMeals = items;
+
+  if (filter.mealType !== '')
+    filteredMeals = data.filter(item => item.mealType.includes(filter.mealType));
+
+  if (filter.cuisineType !== '')
+    filteredMeals = filteredMeals.filter(item => item.cuisine === filter.cuisineType);
+
+  if (filter.cookTime !== '')
+    filteredMeals = filteredMeals.filter(item => item.cookTime === filter.cookTime);
 
   // Render filtered meals
   const dataList = document.getElementById('data-list');
   dataList.innerHTML = '';
   renderItems(filteredMeals);
+  return filteredMeals;
 };
+
 
 const buttonMealTypeList = {
   'breakfast-button': 'Breakfast',
@@ -54,13 +73,43 @@ const buttonMealTypeList = {
   'wraps-button': 'Wraps'
 };
 
+function filterCuisine() {
+  let cuisineType = document.getElementById("cuisine-select").value;
+  searchFilter.cuisineType = cuisineType;
+  renderMeals(items, searchFilter);
+}
 
+function filterCookTime() {
+  let cookTime = document.getElementById("time-select").value;
+  searchFilter.cookTime = cookTime;
+  renderMeals(items, searchFilter);
+}
+
+function filterIngredients() {
+  let cuisineType = document.getElementById("cuisine-select").value;
+  searchFilter.cuisineType = cuisineType;
+  renderMeals(items, searchFilter);
+}
+
+function resetFilters() {
+
+  searchFilter = {
+    mealType: '',
+    cuisineType: '',
+    cookTime: '',
+    ingredients: ''
+  }
+
+  renderMeals(items, searchFilter);
+
+}
 
 
 // Fetch gets your (local) JSON file…
 fetch('assets/data.json')
   .then(response => response.json())
   .then(data => {
+      items = data;
       // Initial rendering of all items
       renderItems(data);
 
@@ -68,7 +117,8 @@ fetch('assets/data.json')
         const button = document.getElementById(buttonId);
         button.addEventListener('click', () => {
           const mealType = buttonMealTypeList[buttonId];
-          renderMealsByType(data, mealType);
+          searchFilter.mealType = mealType;
+          renderMeals(items, searchFilter);
         });
       });
   });
